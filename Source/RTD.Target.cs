c5450ea766a7cae80013b3eb07aabddecddf2eaa
Michael Glover <michaelglovernor@gmail.com>
2024-06-04 20:11:37 -0500

    update build settings

--- Source/RTD.Target.cs
+++ Source/RTD.Target.cs
@@ -8,7 +8,7 @@ public class RTDTarget : TargetRules
 	public RTDTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "RTD" } );
 	}
